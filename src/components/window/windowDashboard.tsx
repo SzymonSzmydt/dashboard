c50d0ab979acd5cbe3e6ec68195f83f7b449ae40
SzymonSzmydt <szymonszmydt@gmail.com>
2023-03-25 19:50:29 +0100

    add user email display

--- src/components/window/windowDashboard.tsx
+++ src/components/window/windowDashboard.tsx
@@ -1,6 +1,8 @@
 import wind from "./window.module.css";
 import WindowDashboardBar from "./windowDashboardBar";
 import { useAuthContext } from "../../context/firebase/AuthContext";
+import userIcon from "../../../public/user.webp";
+import Image from "next/image";
 
 interface WindowProps {
   children: React.ReactNode;
@@ -11,7 +13,9 @@ function WindowDashboard({ children }: WindowProps) {
   return (
     <div className={wind.main}>
       <WindowDashboardBar>
-        <h2> Witaj! </h2> <p>{email?.slice(0, email.indexOf("@"))}</p>
+        <Image src={userIcon} alt='user icon' width={40} height={40} />
+        <h2 className={wind.h2}> Witaj, </h2>
+        <p className={wind.p}>{email?.slice(0, email.indexOf("@"))}</p>
       </WindowDashboardBar>
       <main>{children}</main>
     </div>
